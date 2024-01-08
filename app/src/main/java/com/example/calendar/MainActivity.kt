6f51bc76b350276272d211af5c5a2c91327ce3f6
Andrés Ramos García <anragar20@gmail.com>
2024-01-08 19:41:27 +0100

    Primera Prueba del Calendario

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -1,16 +1,36 @@
-package com.example.calendar
+package com.example.myapplication
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calendar.ui.theme.CalendarTheme
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MiDatePicker()
                 }
             }
         }
@@ -37,10 +57,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CalendarTheme {
-        Greeting("Android")
+fun MiDatePicker(){
+    var fecha by rememberSaveable { mutableStateOf("")}
+    val anio:Int
+    val mes:Int
+    val dia:Int
+    val mCalendar = Calendar.getInstance()
+    anio = mCalendar.get(Calendar.YEAR)
+    mes = mCalendar.get(Calendar.MONTH)
+    dia = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    val mDatePickerDialog = DatePickerDialog(
+        LocalContext.current,
+        {_,anio:Int,mes:Int,dia:Int->
+            fecha = "$dia/${mes+1}/$anio"
+        },anio,mes,dia
+    )
+
+    Box(modifier = Modifier.fillMaxWidth()){
+        Row(modifier = Modifier.align(Alignment.Center)){
+            OutlinedTextField(
+                value = fecha,
+                onValueChange = {fecha = it},
+                readOnly = true,
+                label = { Text(text = "Select Date")}
+            )
+            Icon(
+                imageVector = Icons.Filled.DateRange,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(4.dp)
+                    .clickable {
+                        mDatePickerDialog.show()
+                    }
+            )
+        }
     }
 }
